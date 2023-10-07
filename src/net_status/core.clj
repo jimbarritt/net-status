73ffa4b4e80de09c65084fd6a73f0cd63691ca5e
Jim Barritt <jimb@thoughtworks.com>
2023-10-07 20:20:18 +0100

    Adding basic http get

--- src/net_status/core.clj
+++ src/net_status/core.clj
@@ -1,11 +1,24 @@
-(ns net-status.core)
+(ns net-status.core
+  (:require [clj-http.client :as client]
+            [clojure.java.io :as io]
+            [clojure.core.async :as async]))
 
-(defn ping
+(defn log-response
+  [log-writer url response]
+  (.write log-writer (str (java.util.Date.) " - " url " - " (:status response) "\n")))
+
+(defn head
   "Makes a HEAD call to a URL and returns the results"
-  [url]
-  (println "Going to ping " url))
+  [url log-file]
+  (with-open [log-writer (io/writer log-file :append true)]
+    (let [response (client/get url {:throw-exceptions false})]
+      (log-response log-writer url response))
+    ))
+
+
 
 (defn -main
   "Sets up a polling every 1 second to a url to check the internet status"
   []
   (ping "https://www.google.com"))
+
