31b8e4badcc3740919cd3883259a2788dce0ce5d
Jim Barritt <jimb@thoughtworks.com>
2023-10-07 21:06:02 +0100

    Fully working polling with error handling when network down

--- src/net_status/core.clj
+++ src/net_status/core.clj
@@ -4,21 +4,35 @@
             [clojure.core.async :as async]))
 
 (defn log-response
-  [log-writer url response]
-  (.write log-writer (str (java.util.Date.) " - " url " - " (:status response) "\n")))
+  [log-writer url message]
+  (.write log-writer (str (java.util.Date.) " - " url " - " message "\n")))
 
 (defn head
   "Makes a HEAD call to a URL and returns the results"
-  [url log-file]
-  (with-open [log-writer (io/writer log-file :append true)]
-    (let [response (client/get url {:throw-exceptions false})]
-      (log-response log-writer url response))
+  [url log-writer]
+  (try 
+    (let [response (client/head url {:throw-exceptions false
+                                     :socket-timeout 900
+                                     :connection-timeout 900
+                                     :retry-handler (fn [ex try-count http-context] false)})]
+      (log-response log-writer url (:status response)))
+    (catch Throwable e
+      (log-response log-writer url "NO-NETWORK"))
     ))
 
 
+(defn poll-and-wait
+  [url pause-ms log-file]
+  (with-open [log-writer (io/writer log-file :append true)]
+    (head url log-writer)  
+    (Thread/sleep pause-ms)
+    ))
 
 (defn -main
   "Sets up a polling every 1 second to a url to check the internet status"
   []
-  (ping "https://www.google.com"))
+  (println "Goung to start polling every second now CTRL-C to stop, logs in ~/net-status.log")
+  (while true
+    (poll-and-wait "https://www.google.com" 1000 "/Users/jmdb/net-status.log"))  
+ )
 
